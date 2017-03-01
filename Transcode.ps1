d500cf57bed0e0a83dd8bdcf7db1aac633d167a3
Brandon <willjasen@gmail.com>
2017-03-01 13:34:22 -0500

    removing unneeded code

--- Transcode.ps1
+++ Transcode.ps1
@@ -6,18 +6,15 @@ foreach ($captureFile in Get-ChildItem("$ScreenConnectDir\App_Data\Session\*"))
 {
     Try
     {
-        #Write-Host "." -NoNewLine
         $inputStream = [System.IO.File]::OpenRead($captureFile.FullName)
-		$toolkitInstance = [ScreenConnect.Toolkit]::Instance
-		$toolkitInstance.CreateScreenEncoder()
-		break
-		#$encoder = $toolkitInstance.CreateVideoFileEncoder()
-        #[ScreenConnect.ServerExtensions]::Transcode($inputStream, $encoder, $captureFile.FullName + ".avi")
+	$toolkitInstance = [ScreenConnect.Toolkit]::Instance
+	$encoder = $toolkitInstance.CreateVideoFileEncoder()
+        [ScreenConnect.ServerExtensions]::Transcode($inputStream, $encoder, $captureFile.FullName + ".avi")
     }
     Catch
     {
         echo "Error processing $($captureFile): $($Error[0])"
-		Write-Host "Encountered error, stopping."
-		break
+	Write-Host "Encountered error, stopping."
+	break
     }
 }
