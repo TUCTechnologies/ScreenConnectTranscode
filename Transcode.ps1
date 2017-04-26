ca5b3347e6d40243fbc7680cd7dc4946f2206a0d
Brandon <willjasen@gmail.com>
2017-04-26 13:58:50 -0400

    Moving transcode portion into its own function
    Moving transcode portion into its own function, try multiple times to transcode the file in the event that the output is too small

--- Transcode.ps1
+++ Transcode.ps1
@@ -3,12 +3,67 @@ $ScreenConnectDir = "C:\Program Files (x86)\ScreenConnect"
 $SaveDirectory = "C:\Transcoded Sessions"
 Add-Type -Path (Get-Item "$ScreenConnectDir\bin\ScreenConnect.Core.dll").FullName
 Add-Type -Path (Get-Item "$ScreenConnectDir\bin\ScreenConnect.Server.dll").FullName
+$TranscodeAttempts=0
 
 # Returns true if a transcode has already happened (based on filename)
 Function ExistingTranscode($fileNameToTest)
 {
 	# Returns true if the file exists
 	Return (Test-Path "$SaveDirectory\$fileNameToTest")
+}
+
+# Transcodes a raw capture file to its output
+Function Transcode
+{
+	Param(
+		[Parameter(Mandatory=$false,Position=0)] $rawCapturePath,
+		[Parameter(Mandatory=$false,Position=1)] $outputFileName
+	)
+	
+	# If a transcode hasn't happened yet, do it
+	If(-Not(ExistingTranscode($outputFileName)))
+	{
+	    Write-Host "Transcoding $outputFileName"
+		
+		Try
+		{
+			# Attempt the transcode
+			$inputStream = [System.IO.File]::OpenRead($rawCapturePath)
+			$toolkitInstance = [ScreenConnect.ServerToolkit]::Instance
+			$encoder = $toolkitInstance.CreateVideoFileEncoder()
+			[ScreenConnect.ServerExtensions]::Transcode($inputStream, $encoder, "$SaveDirectory\" + $outputFileName)
+		}
+		Catch
+		{
+			# Display encountered errors
+			echo "Error processing $($captureFile): $($Error[0])"
+		}
+		Finally
+		{
+			# Close the source file reader
+			$inputStream.Dispose()
+		}
+	}
+	Else
+	{
+		# If the file exists, but it is less than 6000 bytes, redo it
+		If((Get-Item ("$SaveDirectory\" + $outputFileName)).Length -lt 6000)
+		{
+		    If($TranscodeAttempts -lt 3)
+			{
+				Write-Host "Removing item... $outputFileName"
+				Remove-Item ("$SaveDirectory\" + $outputFileName)
+				Transcode -rawCapturePath $rawCapturePath -outputFileName $outputFileName
+				$TranscodeAttempts += 1
+			}
+			Else
+			{
+				$TranscodeAttempts=0
+				Break
+			}
+		}
+		
+	}
 }
 
 # Loop through the raw captures
@@ -49,32 +104,5 @@ ForEach ($captureFile in $files)
 	# Format the filename as timestamp_SessionGUID_CaptureGUID.avi
 	$outputFileName = $timestamp + "_" + $SessionGUID + "_" + $CaptureGUID + ".avi"
 	
-	# If a transcode hasn't happened yet, do it
-	If(-Not(ExistingTranscode($outputFileName)))
-	{
-	    Write-Host "Transcoding $outputFileName"
-		
-		Try
-		{
-			# Attempt the transcode
-			$inputStream = [System.IO.File]::OpenRead($captureFilePath)
-			$toolkitInstance = [ScreenConnect.ServerToolkit]::Instance
-			$encoder = $toolkitInstance.CreateVideoFileEncoder()
-			[ScreenConnect.ServerExtensions]::Transcode($inputStream, $encoder, "$SaveDirectory\" + $outputFileName)
-		}
-		Catch
-		{
-			# Display encountered errors
-			echo "Error processing $($captureFile): $($Error[0])"
-		}
-		Finally
-		{
-			# Close the source file reader
-			$inputStream.Dispose()
-		}
-	}
-	Else
-	{
-		Write-Host "The file at $timestamp has already been transcoded."
-	} 
+	Transcode -rawCapturePath $captureFilePath -outputFileName $outputFileName
 }
